aba7670dd5ece062e31d27d8878196f000706053
zehui <sbura@sina.com>
2023-01-07 19:56:16 +0800

    feat✨:  搜索列表的基本内容

--- components/search-history/search-history.vue
+++ components/search-history/search-history.vue
@@ -1,19 +1,36 @@
 <template>
-  <view>
-    搜索历史
+  <view class="search-history-container">
+    <view class="search-history-title-box">
+      <view class="search-history-title"></view>
+      <view v-if="!isShowClear">
+        <uni-icons type="trash" @click="isShowClear = true"></uni-icons>
+      </view>
+      <view v-else>
+        <text class="txt">全部删除</text>
+        <text class="txt" @click="isShowClear = false">完成</text>
+      </view>
+
+      <view class="search-history-box">
+        <block v-for="(item, index) in searchData" :key="index">
+          <view class="search-history-item">
+            <text class="history-txt line-clamp">{{ item }}</text>
+            <uni-icons v-show="isShowClear" type="clear"></uni-icons>
+          </view>
+        </block>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-
-      };
-    }
-  }
+export default {
+  data() {
+    return {
+      isShowClear: false,
+      searchData: ["sunday", "uniapp", "vue"],
+    };
+  },
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
