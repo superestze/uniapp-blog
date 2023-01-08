63a7aa03fbdfd724f26ecf975234809f4c0617e3
zehui <sbura@sina.com>
2023-01-08 14:45:38 +0800

    数据持久化

--- store/modules/search.js
+++ store/modules/search.js
@@ -1,11 +1,18 @@
+const STORAGE_KEY = 'search-list'
 export default {
   namespaced: true,
   state: () => {
     return {
-      searchData: []
+      searchData: uni.getStorageSync(STORAGE_KEY) || []
     }
   },
   mutations: {
+    saveToStorage(state) {
+      uni.setStorage({
+        key: STORAGE_KEY,
+        data: state.searchData
+      })
+    },
     addSearchData(state, val) {
       if (!val) return
       const index = state.searchData.findIndex((item) => item === val);
@@ -13,12 +20,15 @@ export default {
         state.searchData.splice(index, 1);
       }
       state.searchData.unshift(val);
+      this.commit('search/saveToStorage')
     },
     removeSearchData(state, index) {
       state.searchData.splice(index, 1);
+      this.commit('search/saveToStorage')
     },
     removeAllSearchData(state) {
       state.searchData = [];
+      this.commit('search/saveToStorage')
     }
   }
 }
